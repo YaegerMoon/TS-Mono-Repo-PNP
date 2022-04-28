e95c019985743d632d73b9b31a71ac19479f795f
YaegerMoon <yaeger@kakao.com>
2022-04-29 02:10:19 +0900

    Make Change on Theme

--- packages/theme/src/main.ts
+++ packages/theme/src/main.ts
@@ -1,9 +1,11 @@
 interface ThemeTemplate {
   mainColor: string;
+  subColor: string;
 }
 
 const defaultTheme: ThemeTemplate = {
   mainColor: "#909090",
+  subColor: "#99092",
 };
 
 export { type ThemeTemplate, defaultTheme };
