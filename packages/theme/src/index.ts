4ae838acf9f47ecdbdb753590b66dc340ae477e7
YaegerMoon <yaeger@kakao.com>
2022-04-30 16:15:54 +0900

    Make ignore file of prettier

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -1,11 +1,11 @@
 interface ThemeTemplate {
-	mainColor: string;
-	subColor: string;
+  mainColor: string;
+  subColor: string;
 }
 
 const defaultTheme: ThemeTemplate = {
-	mainColor: '#909090',
-	subColor: '#99092',
+  mainColor: '#909090',
+  subColor: '#99092',
 };
 
 export { type ThemeTemplate, defaultTheme };
